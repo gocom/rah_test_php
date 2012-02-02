7de8fdae7c21dd78d7b66b09244df3fb897e51ee
Jukka Svahn <web@rahinaa.biz>
2012-02-02 06:46:17 +0200

    Adding v0.2 from SVN.

--- code.php
+++ code.php
@@ -1,29 +1,130 @@
 <?php	##################
-	#
-	#	rah_test_php-plugin for Textpattern
-	#	version 0.1
+	#	Rah_test_php-plugin for Textpattern
+	#	version 0.2
 	#	by Jukka Svahn
 	#	http://rahforum.biz
 	#
-	###################
+	#	Copyright (C) 2011 Jukka Svahn <http://rahforum.biz>
+	#	Licensed under GNU Genral Public License version 2
+	#	http://www.gnu.org/licenses/gpl-2.0.html
+	#
+	##################
 
-	if (@txpinterface == 'admin') {
-		add_privs('rah_test_php', '1,2');
-		register_tab("extensions", "rah_test_php", "Test PHP");
-		register_callback("rah_test_php", "rah_test_php");
+	if(@txpinterface == 'admin') {
+		add_privs('rah_test_php','1,2');
+		add_privs('plugin_prefs.rah_test_php','1,2');
+		register_callback('rah_test_php_prefs','plugin_prefs.rah_test_php');
+		register_tab('extensions','rah_test_php',gTxt('rah_test_php') == 'rah_test_php' ? 'Execute PHP' : gTxt('rah_test_php'));
+		register_callback('rah_test_php','rah_test_php');
+		register_callback('rah_test_php_head','admin_side','head_end');
 	}
 
+/**
+	The pain and main function
+*/
+
 	function rah_test_php() {
-		pagetop('rah_test_php');
-		include_once txpath.'/publish.php';
-		echo 
-			'<form method="post" action="index.php" style="width:800px;margin:0 auto;">'.n.
-			'	<h1>rah_test_php: Test PHP scripts</h1>'.n.
-			'	<input type="hidden" name="event" value="rah_test_php" />'.n.
-			'	<input type="hidden" name="test_do" value="1" />'.n.
-			'	<label for="php">PHP code:</label>'.n.
-			'	<textarea id="php" name="php" rows="10" cols="30" style="width:790px;"></textarea>'.n.
-			'	<button type="submit" style="margin: 10px;">Run PHP code</button>'.n.
-			((ps('php') && ps('test_do')) ? '<hr /><p>PHP returned:</p><pre>'.parse('<txp:php> '.ps('php'). ' </txp:php>').'</pre>'.n : '').
+		
+		require_privs('rah_test_php');
+		
+		global $event, $textarray;
+
+		/*
+			Make sure language strings are set
+		*/
+		
+		foreach(
+			array(
+				'rah_test_php' => 'Execute PHP',
+				'rah_test_php_code' => 'PHP code',
+				'rah_test_php_run' => 'Run the PHP',
+				'rah_test_php_returned' => 'Returned'
+			) as $string => $translation
+		)
+			if(!isset($textarray[$string]))
+				$textarray[$string] = $translation;
+		
+		pagetop(gTxt('rah_test_php'));
+		
+		echo
+			'<form method="post" action="index.php" id="rah_test_php_container" class="rah_ui_container">'.n.
+			'	<input type="hidden" name="event" value="'.$event.'" />'.n.
+			'	<p>'.n.
+			'		<label>'.n.
+			'			'.gTxt('rah_test_php_code').'<br />'.n.
+			'			<textarea class="code" name="php" rows="10" cols="50">'.htmlspecialchars(ps('php')).'</textarea>'.n.
+			'		</label>'.n.
+			'	</p>'.n.
+			'	<p>'.n.
+			'		<input type="submit" value="'.gTxt('rah_test_php_run').'" class="publish" />'.n.
+			'	</p>'.n;
+
+		if(ps('php')) {
+			
+			ob_start();
+			eval(ps('php'));
+			$returned = ob_get_clean();
+			
+			echo
+				'	<p><strong>'.gTxt('rah_test_php_returned').'</strong></p>'.n.
+				'	<pre id="rah_test_php_pre">'.htmlspecialchars($returned).'</pre>'.n;
+		}
+		
+		echo
 			'</form>';
-	}?>
+		
+	}
+
+/**
+	Adds styles and JavaScript to the <head>
+*/
+
+	function rah_test_php_head() {
+		
+		global $event;
+		
+		if($event != 'rah_test_php')
+			return;
+		
+		$string = gTxt('are_you_sure');
+		
+		echo <<<EOF
+			<style type="text/css">
+				#rah_test_php_container {
+					width: 650px;
+					margin: 0 auto;
+				}
+				#rah_test_php_container textarea {
+					width: 100%;
+				}
+				#rah_test_php_pre {
+					white-space: pre;
+					overflow: auto;
+				}
+			</style>
+			<script type="text/javascript">
+				<!--
+				$(document).ready(function(){
+					$('form#rah_test_php_container').submit(
+						function() {
+							return verify('{$string}');
+						}
+					)
+				});
+				-->
+			</script>
+EOF;
+	}
+
+/**
+	Redirect to the prefs pane
+*/
+
+	function rah_test_php_prefs() {
+		header('Location: ?event=rah_test_php');
+		echo 
+			'<p>'.n.
+			'	<a href="?event=rah_test_php">'.gTxt('continue').'</a>'.n.
+			'</p>';
+	}
+?>
